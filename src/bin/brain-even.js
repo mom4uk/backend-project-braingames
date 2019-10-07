8620e40cc0cae9e613becb49a7bb4738c1709796
manov <imanovplus@gmail.com>
2019-10-07 20:46:53 +0300

    finish 5 step

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,18 +1,18 @@
 import readlineSync from 'readline-sync';
 
 import {
-  findOutTheName, findOutAnEvenNumber, randomInteger, greeting, abstract,
+  findOutAnEvenNumber, randomInteger, greeting, abstract,
 } from '..';
 
 greeting();
 
 abstract('Answer "yes" if the number is even, otherwise answer "no".');
 
-findOutTheName();
 
 let counterRightAnswer = 0;
 
 const parityCheck = () => {
+  const userName = readlineSync.question('May I have your name? ');
   while (counterRightAnswer !== 3) {
     const randomNumber = randomInteger(1, 100);
     console.log(`Question: ${randomNumber}`);
@@ -22,22 +22,22 @@ const parityCheck = () => {
       console.log('Correct!');
     } else if (findOutAnEvenNumber(randomNumber) === true && userAnswer === 'no') {
       counterRightAnswer = 0;
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${findOutTheName.userName}!`);
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`);
     } else if (findOutAnEvenNumber(randomNumber) === true && userAnswer !== 'no') {
       counterRightAnswer = 0;
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${findOutTheName.userName}!`);
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`);
     }
     if (findOutAnEvenNumber(randomNumber) === false && userAnswer === 'no') {
       counterRightAnswer += 1;
       console.log('Correct!');
     } else if (findOutAnEvenNumber(randomNumber) === false && userAnswer === 'yes') {
       counterRightAnswer = 0;
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${findOutTheName.userName}!`);
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
     } else if (findOutAnEvenNumber(randomNumber) === false && userAnswer !== 'no') {
       counterRightAnswer = 0;
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${findOutTheName.userName}!`);
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
     }
   }
-  return console.log(`Congratulations, ${findOutTheName.userName}!`);
+  return console.log(`Congratulations, ${userName}!`);
 };
 parityCheck();
