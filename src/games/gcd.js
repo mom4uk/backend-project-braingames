9f7d7b452cc832e80d741c05bba3cefca491c11d
manov <imanovplus@gmail.com>
2019-10-24 22:13:49 +0300

    start step 7

---
+++ src/games/gcd.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import {
+  frame, randomInteger,
+} from '..';
+
+const textDescription = 'Find the greatest common divisor of given numbers.';
+
+const getTaskAndCorrectAnswer = () => {
+  const firstRandNum = randomInteger(1, 100);
+  const secondRandNum = randomInteger(1, 100);
+  const question = `${firstRandNum} ${secondRandNum}`;
+  const answer = (firstNum, secondNum) => {
+    if (firstNum %) {
+
+    }
+  };
+
+  const getCorrectAnswer = 
+};
