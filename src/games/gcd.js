8834141f5012602ce6f7ebb2615eeb9eabde83e5
manov <imanovplus@gmail.com>
2019-11-30 23:18:15 +0300

    fix after check

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,17 +2,17 @@ import engine from '..';
 import randomValue from '../utils';
 
 const descriptionTask = 'Find the greatest common divisor of given numbers.';
-const getGreatestCommonDenom = (firstValue, secondValue) => {
+const getGreatestCommonDivisor = (firstValue, secondValue) => {
   if (firstValue % secondValue === 0) {
     return secondValue;
   }
-  return getGreatestCommonDenom(secondValue, firstValue % secondValue);
+  return getGreatestCommonDivisor(secondValue, firstValue % secondValue);
 };
 const getTaskAndCorrectAnswer = () => {
   const firstRandomValue = randomValue(1, 100);
   const secondRandomValue = randomValue(1, 100);
   const question = `${firstRandomValue} ${secondRandomValue}`;
-  const correctAnswer = String(getGreatestCommonDenom(firstRandomValue, secondRandomValue));
+  const correctAnswer = String(getGreatestCommonDivisor(firstRandomValue, secondRandomValue));
   return [question, correctAnswer];
 };
 const startGameGcd = () => engine(descriptionTask, getTaskAndCorrectAnswer);
--- src/games/progression.js
+++ src/games/progression.js
@@ -2,32 +2,27 @@ import engine from '..';
 import randomValue from '../utils';
 
 const descriptionTask = 'What number is missing in the progression?';
-const lengthProgression = 10;
 const getArithmeticProgression = (progressionMember, progressionDifference) => {
-  const valuesProgression = [];
+  const lengthProgression = 10;
+  const progression = [];
   for (let i = 0; i <= lengthProgression - 1; i += 1) {
-    valuesProgression.push(progressionMember + progressionDifference * i);
+    progression.push(progressionMember + progressionDifference * i);
   }
-  return valuesProgression;
+  return progression;
 };
 const getTaskAndCorrectAnswer = () => {
-  let replacedValueInArray = '';
-  const progressionMember = randomValue(0, 100);
+  const lengthProgression = 10;
+  const initialProgressionMember = randomValue(0, 100);
   const progressionDifference = randomValue(0, 100);
-  const randomIndex = randomValue(0, lengthProgression);
-  const valuesArithmeticProgression = getArithmeticProgression(progressionMember,
+  const randomIndex = randomValue(0, lengthProgression - 1);
+  const arithmeticProgression = getArithmeticProgression(initialProgressionMember,
     progressionDifference);
   const getQuestion = (array) => {
-    for (let i = 0; i <= array.length - 1; i += 1) {
-      if (randomIndex === i) {
-        replacedValueInArray = `${progressionMember + progressionDifference * i}`;
-      }
-    }
     array.splice(randomIndex, 1, '..');
     return array.join(' ');
   };
-  const question = getQuestion(valuesArithmeticProgression);
-  const correctAnswer = replacedValueInArray;
+  const question = getQuestion(arithmeticProgression);
+  const correctAnswer = `${initialProgressionMember + progressionDifference * randomIndex}`;
   return [question, correctAnswer];
 };
 const startGameProgression = () => engine(descriptionTask, getTaskAndCorrectAnswer);
