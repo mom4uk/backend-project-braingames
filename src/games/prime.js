e95e8cec3c779d3093878cabc4671b8e21c2099a
manov <imanovplus@gmail.com>
2019-12-06 00:32:13 +0300

    fix name and move const

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,16 +4,17 @@ import randomValue from '../utils';
 const descriptionTask = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (value) => {
-  for (let i = 2; i <= value / 2; i += 1) {
+  for (let i = 0; i <= value / 2; i += 1) {
+    if (value < 2) {
+      return false;
+  }
     if (value % i === 0) {
       return false;
     }
   }
-  if (value < 2) {
-    return false;
-  }
   return true;
 };
+console.log(isPrime(0),isPrime(32),isPrime(25),isPrime(31),isPrime(23),isPrime(37),isPrime(61),isPrime(67));
 const getTaskAndCorrectAnswer = () => {
   const question = randomValue(1, 100);
   const correctAnswer = isPrime(question) ? 'yes' : 'no';
--- src/games/progression.js
+++ src/games/progression.js
@@ -2,6 +2,7 @@ import engine from '..';
 import randomValue from '../utils';
 
 const descriptionTask = 'What number is missing in the progression?';
+const lengthArithmericProgression = 10;
 const getArithmeticProgression = (progressionMember, progressionDifference, lengthProgression) => {
   const progression = [];
   for (let i = 0; i <= lengthProgression - 1; i += 1) {
@@ -10,18 +11,17 @@ const getArithmeticProgression = (progressionMember, progressionDifference, leng
   return progression;
 };
 const getTaskAndCorrectAnswer = () => {
-  const lengthProgression = 10;
   const initialProgressionMember = randomValue(0, 100);
   const progressionDifference = randomValue(0, 100);
-  const randomIndex = randomValue(0, lengthProgression - 1);
+  const hiddenElementIndex = randomValue(0, lengthArithmericProgression - 1);
   const arithmeticProgression = getArithmeticProgression(initialProgressionMember,
-    progressionDifference, lengthProgression);
+    progressionDifference, lengthArithmericProgression);
   const getQuestion = (array) => {
-    array.splice(randomIndex, 1, '..');
+    array.splice(hiddenElementIndex, 1, '..');
     return array.join(' ');
   };
   const question = getQuestion(arithmeticProgression);
-  const correctAnswer = `${initialProgressionMember + progressionDifference * randomIndex}`;
+  const correctAnswer = `${initialProgressionMember + progressionDifference * hiddenElementIndex}`;
   return [question, correctAnswer];
 };
 const startGameProgression = () => engine(descriptionTask, getTaskAndCorrectAnswer);
