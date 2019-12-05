75c4dc74d2ec79fd3e923f5d181349dd711580f9
manov <imanovplus@gmail.com>
2019-12-06 00:44:18 +0300

    delete function theQuestion

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,24 +3,21 @@ import randomValue from '../utils';
 
 const descriptionTask = 'What number is missing in the progression?';
 const lengthArithmericProgression = 10;
+const hiddenElementIndex = randomValue(0, lengthArithmericProgression - 1);
 const getArithmeticProgression = (progressionMember, progressionDifference, lengthProgression) => {
   const progression = [];
   for (let i = 0; i <= lengthProgression - 1; i += 1) {
     progression.push(progressionMember + progressionDifference * i);
   }
-  return progression;
+  progression.splice(hiddenElementIndex, 1, '..');
+  return progression.join(' ');
 };
 const getTaskAndCorrectAnswer = () => {
   const initialProgressionMember = randomValue(0, 100);
   const progressionDifference = randomValue(0, 100);
-  const hiddenElementIndex = randomValue(0, lengthArithmericProgression - 1);
   const arithmeticProgression = getArithmeticProgression(initialProgressionMember,
     progressionDifference, lengthArithmericProgression);
-  const getQuestion = (array) => {
-    array.splice(hiddenElementIndex, 1, '..');
-    return array.join(' ');
-  };
-  const question = getQuestion(arithmeticProgression);
+  const question = arithmeticProgression;
   const correctAnswer = `${initialProgressionMember + progressionDifference * hiddenElementIndex}`;
   return [question, correctAnswer];
 };
